35941cd1f266d4f5121f834fbd7551e192d6c7b2
David Hamner <ke7oxh@gmail.com>
2025-01-16 17:07:24 -0800

    1/2 done controls

--- player.gd
+++ player.gd
@@ -9,12 +9,24 @@ extends CharacterBody3D
 const SPEED = 5.0
 const LERP_VAL = .15
 
+
+var action_state = "walk"
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("roll"):
+		action_state = "roll"
+		anim_tree.set("parameters/roll/request", true)
+		 
+	if Input.is_action_just_released("roll"):
+		action_state = "walk"
+	
+	if Input.is_action_just_released("attack"):
+		anim_tree.set("parameters/shooting/blend_amount", 0.0)
 		
 	if event is InputEventMouseMotion:
 		spring_arm_pivot.rotate_y(-event.relative.x * .005)
@@ -25,6 +37,12 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+		
+	if Input.is_action_pressed("attack"):
+		if action_state == "walk":
+			var lerp_to_1 = lerpf(anim_tree.get("parameters/shooting/blend_amount"), 1.0, get_process_delta_time() * 20)
+			anim_tree.set("parameters/shooting/blend_amount", lerp_to_1)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
