b90ea21d71711d1f7bb9e1cd4e9c5416ab0e3067
David Hamner <ke7oxh@gmail.com>
2025-01-17 12:14:29 -0800

    Better roll

--- player.gd
+++ player.gd
@@ -12,14 +12,13 @@ extends CharacterBody3D
 @onready var leaf3 = $player/Armature/Skeleton3D/leafs/leaf3
 @onready var leaf4 = $player/Armature/Skeleton3D/leafs/leaf4
 
-
-const ROLL_SPEED = 6.0
+const ROLL_SPEED = 5.5
 const SPEED = 5.0
 const LERP_VAL = .15
 const ROLL_ROTATION_SPEED = 10.0
-const ROLL_HEIGHT = 1.5
-const ROLL_SPEED_FALLOFF = 0.5
+const ROLL_HEIGHT = 3
 const SPIN_ROTATION_SPEED = 15.0
+const ROLL_DURATION = 0.4  # Fixed duration for roll in seconds
 
 enum ActionState {IDLE, WALK, ROLL, ATTACK, SPIN}
 
@@ -30,9 +29,8 @@ var is_rolling = false
 var is_spinning = false
 var roll_direction = Vector3.ZERO
 var initial_roll_rotation = 0.0
-var total_roll_rotation = 0.0
+var roll_timer = 0.0
 var initial_mesh_position = Vector3.ZERO
-var current_roll_speed = 0.0
 var spin_rotation = 0.0
 var prev_is_spinning = false
 
@@ -61,20 +59,11 @@ func _unhandled_input(event):
 			roll_direction = direction
 			armature.rotation.y = atan2(direction.x, direction.z)
 			initial_roll_rotation = armature.rotation.y
-			total_roll_rotation = 0.0
-			current_roll_speed = ROLL_SPEED
+			roll_timer = 0.0
 			animation_tree.set("parameters/roll/request", true)
 	
 	if Input.is_action_just_pressed("attack") and action_state != ActionState.SPIN:
 		action_state = ActionState.ATTACK
-	
-	if Input.is_action_just_released("roll"):
-		if action_state == ActionState.ROLL:
-			action_state = ActionState.IDLE
-			roll_direction = Vector3.ZERO
-			mesh.rotation = Vector3(0, -PI, 0)
-			mesh.position = initial_mesh_position
-			current_roll_speed = 0.0
 	
 	if Input.is_action_just_released("attack"):
 		if action_state == ActionState.ATTACK:
@@ -148,25 +137,22 @@ func _physics_process(delta: float) -> void:
 			velocity.z = lerp(velocity.z, 0.0, LERP_VAL)
 	else:
 		if is_rolling:
-			direction = roll_direction * current_roll_speed
+			direction = roll_direction * ROLL_SPEED
 			armature.rotation.y = initial_roll_rotation
-			total_roll_rotation += ROLL_ROTATION_SPEED * delta
+			roll_timer += delta
 			
-			var roll_progress = total_roll_rotation / (2 * PI)
+			var roll_progress = roll_timer / ROLL_DURATION
+			var rotation_angle = roll_progress * (2 * PI)
 			
-			if total_roll_rotation >= 2 * PI:
-				total_roll_rotation = 0.0
+			if roll_timer >= ROLL_DURATION:
+				roll_timer = 0.0
 				mesh.rotation = Vector3(0, -PI, 0)
 				mesh.position = initial_mesh_position
-				current_roll_speed = 0.0
 				action_state = ActionState.IDLE
 			else:
-				mesh.rotation = Vector3(total_roll_rotation, -PI, 0)
+				mesh.rotation = Vector3(rotation_angle, -PI, 0)
 				var height_offset = sin(roll_progress * PI) * ROLL_HEIGHT
 				mesh.position = initial_mesh_position + Vector3(0, height_offset, 0)
-				
-				if roll_progress > 0.6:
-					current_roll_speed = lerp(current_roll_speed, SPEED, ROLL_SPEED_FALLOFF * delta)
 		else:
 			direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 			direction = direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
@@ -183,7 +169,7 @@ func _physics_process(delta: float) -> void:
 			if action_state == ActionState.ATTACK:
 				target_speed *= 0.5
 			elif is_rolling:
-				target_speed = current_roll_speed
+				target_speed = ROLL_SPEED
 				
 			velocity.x = lerp(velocity.x, direction.x * target_speed, LERP_VAL)
 			velocity.z = lerp(velocity.z, direction.z * target_speed, LERP_VAL)
