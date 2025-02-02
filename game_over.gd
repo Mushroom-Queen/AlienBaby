7f251e3cb7d32c9a19b807ed1f4c113516e1dfa1
David Hamner <ke7oxh@gmail.com>
2025-02-02 09:34:43 -0800

    add restart

--- game_over.gd
+++ game_over.gd
@@ -3,12 +3,17 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
+		reload()
 
+
+func reload():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	get_tree().change_scene_to_file("res://menu.tscn")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
